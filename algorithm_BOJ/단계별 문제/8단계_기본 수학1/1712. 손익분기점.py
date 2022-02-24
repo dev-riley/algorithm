5bfc9ca6964a75e4c4bae22ce9a2f0f5aedd0d8e
helen121027 <helen121027@gmail.com>
2022-02-24 23:39:10 +0900

    220224

--- algorithm_BOJ/단계별 문제/8단계_기본 수학1/1712. 손익분기점.py
+++ algorithm_BOJ/단계별 문제/8단계_기본 수학1/1712. 손익분기점.py
@@ -1,2 +1,12 @@
-A, B, C = input().split()
+A, B, C = list(map(int, input().split()))
 
+def ceil(n):
+    if n == int(n):
+        return n
+    else:
+        return int(n) + 1
+
+if A / (C - B) >= 0:
+    print(round(ceil(A / (C - B)) + 1))
+else:
+    print(-1)
