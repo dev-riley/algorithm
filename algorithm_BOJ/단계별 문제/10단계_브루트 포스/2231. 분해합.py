7794b83077f0111c9534656583abd0d89f375a1a
helen121027 <helen121027@gmail.com>
2022-05-10 00:19:51 +0900

    220510 2231번

---
+++ algorithm_BOJ/단계별 문제/10단계_브루트 포스/2231. 분해합.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+result = 0
+for i in range(1, n + 1):
+     a = list(map(int, str(i)))
+     print(a)
+     s = i + sum(a)
+     if s == n:
+         result = i
+         break
+
+print(result)
