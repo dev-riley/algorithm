2459fe9e89eadbbfcb96e78b4d78ab5aa3c93e96
helen121027 <helen121027@gmail.com>
2022-07-24 18:18:36 +0900

    220723

---
+++ algorithm_BOJ/패스트캠퍼스/자료구조/2-3. 키로거.py
@@ -0,0 +1,21 @@
+import sys
+TC = int(input())
+
+for tc in TC:
+    left_stack = []
+    right_stack = []
+    data = input()
+    for i in data:
+        if i == '-':
+            if left_stack:
+                left_stack.pop()
+        elif i == '<':
+            if left_stack:
+                right_stack.append(left_stack.pop())
+        elif i == '>':
+            if right_stack:
+                left_stack.append(right_stack.pop())
+        else:
+            left_stack.append(i)
+        left_stack.extend(reversed(right_stack))
+        print(''.join(left_stack))
