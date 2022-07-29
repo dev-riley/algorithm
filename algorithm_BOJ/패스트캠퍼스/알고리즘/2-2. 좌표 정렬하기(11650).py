f2ff795a5fbdd37c77430e51f17dcf97a9b463fd
helen121027 <helen121027@gmail.com>
2022-07-30 02:55:05 +0900

    220729 11650 좌표 정렬하기

---
+++ algorithm_BOJ/패스트캠퍼스/알고리즘/2-2. 좌표 정렬하기(11650).py
@@ -0,0 +1,13 @@
+# ----- 이미 푼 문제 -----
+n = int(input())
+
+array = []
+
+for _ in range(n):
+    x, y = map(int, input().split(' '))
+    array.append((x, y))
+
+array = sorted(array)
+
+for i in array:
+    print(i[0], i[1])
