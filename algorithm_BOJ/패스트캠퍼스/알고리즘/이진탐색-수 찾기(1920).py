ab61fdc3f1473f2a9a447fcaea0cdce5f37f34c6
helen121027 <helen121027@gmail.com>
2022-07-30 02:55:18 +0900

    220729 소스코드 추가

--- algorithm_BOJ/패스트캠퍼스/알고리즘/이진탐색-수 찾기(1920).py
+++ algorithm_BOJ/패스트캠퍼스/알고리즘/이진탐색-수 찾기(1920).py
@@ -20,3 +20,15 @@ for item in M_list:
         print(1)
     else:
         print(0)
+
+#------set자료형으로 풀어보기(더 간단)------
+n = int(input())
+array = set(map(int, input().split()))
+m = int(input())
+x = list(map(int, input().split()))
+
+for i in x:
+    if i not in array:
+        print('0')
+    else:
+        print('1')
