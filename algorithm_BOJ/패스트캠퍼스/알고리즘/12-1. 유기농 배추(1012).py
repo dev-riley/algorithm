1a922c1e75bd31531b211e61103d3afc78ea74a2
helen121027 <helen121027@gmail.com>
2022-09-05 01:27:18 +0900

    220905 1012 유기농 배추

---
+++ algorithm_BOJ/패스트캠퍼스/알고리즘/12-1. 유기농 배추(1012).py
@@ -0,0 +1,29 @@
+import sys
+sys.setrecursionlimit(100000)
+
+def dfs(x, y):
+    visited[x][y] = True
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    for dx, dy in directions:
+        nx, ny = x + dx, y + dy
+        if nx < 0 or nx >= n or ny < 0 or ny >= m:
+            continue
+        if array[nx][ny] and not visited[nx][ny]:
+            dfs(nx, ny)
+
+T = int(input())
+for _ in range(T):
+    # m : 가로길이, n : 세로길이, k : 배추 위치 갯수
+    m, n, k = map(int, input().split())
+    array = [[0] * m for _ in range(n)]
+    visited = [[False] * m for _ in range(n)]
+    for _ in range(k):
+        y, x = map(int, input().split())
+        array[x][y] = 1
+    result = 0
+    for i in range(n):
+        for j in range(m):
+            if array[i][j] and not visited[i][j]:
+                dfs(i, j)
+                result += 1
+    print(result)
