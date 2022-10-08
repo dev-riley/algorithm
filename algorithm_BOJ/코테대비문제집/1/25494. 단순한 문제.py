243a2f7d6e5b721454b713c96e9a98c252844021
helen121027 <helen121027@gmail.com>
2022-10-09 01:30:54 +0900

    221009 25494 단순한 문제

--- algorithm_BOJ/코테대비문제집/1/25494. 단순한 문제.py
+++ algorithm_BOJ/코테대비문제집/1/25494. 단순한 문제.py
@@ -1,5 +1,9 @@
-n = int(input())
-
-for _ in range(int(n)):
-  data = map(int, input().split())
-print(data)
+for t in range(int(input())):
+    ans = 0
+    x, y, z = map(int, input().split())
+    for i in range(1, x + 1):
+        for j in range(1, y + 1):
+            for k in range(1, z + 1):
+                if i % j == j % k == k % i:
+                    ans += 1
+    print(ans)
