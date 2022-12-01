d7234957a95bbd348677dad40e89c48f7ddddc76
helen121027 <helen121027@gmail.com>
2022-12-01 22:20:15 +0900

    221201 10814 주석 추가

--- algorithm_BOJ/단계별 문제/11단계_정렬/10814. 나이순 정렬.py
+++ algorithm_BOJ/단계별 문제/11단계_정렬/10814. 나이순 정렬.py
@@ -6,5 +6,7 @@ for i in range(n):
     data.append(sys.stdin.readline())
 
 data.sort(key=lambda x: (int(x.split()[0])))
-
+print(data) # ['20 Sunyoung\n', '21 Junkyu\n', '21 Dohyun\n']
 print(''.join(data))
+# for i in data:
+#     print(i)
