100374b0f7927269def033ac712789d4bbf471a6
helen121027 <helen121027@gmail.com>
2022-12-12 22:47:52 +0900

    221212 2164 카드 2

---
+++ algorithm_BOJ/단계별 문제/큐, 덱/2164. 카드 2.py
@@ -0,0 +1,13 @@
+from collections import deque
+n = int(input())
+card = deque()
+for i in range(1, n + 1):
+    card.append(i)
+
+while len(card) >= 1:
+    if len(card) == 1:
+        print(*card)
+        break
+    card.popleft()
+    card.append(card.popleft())
+
