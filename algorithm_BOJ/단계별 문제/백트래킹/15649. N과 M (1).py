4510b539f74796f02389a63f9e27a9970930aad8
helen121027 <helen121027@gmail.com>
2023-01-30 21:57:07 +0900

    230130 15649 N과 M (1)

---
+++ algorithm_BOJ/단계별 문제/백트래킹/15649. N과 M (1).py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split())
+visited = [False] * n
+arr = []
+
+def solve(depth, n, m):
+    if depth == m:
+        print(*arr)
+        return
+    for i in range(n):
+        if not visited[i]:
+            visited[i] = True
+            arr.append(i + 1)
+            solve(depth + 1, n, m)
+            visited[i] = False
+            arr.pop()
+
+solve(0, n, m)
