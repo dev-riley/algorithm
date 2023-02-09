91353f5c88f910206c3d636361a4b9811420283e
helen121027 <helen121027@gmail.com>
2023-02-09 21:40:36 +0900

    230209 24480 주석 추가

--- algorithm_BOJ/단계별 문제/그래프와 순회/24480. 알고리즘 수업 - 깊이 우선 탐색 2.py
+++ algorithm_BOJ/단계별 문제/그래프와 순회/24480. 알고리즘 수업 - 깊이 우선 탐색 2.py
@@ -1,26 +1,30 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10 ** 5)
+sys.setrecursionlimit(10 ** 5) # 재귀 허용 깊이를 수동으로 늘려주는 코드
 
 n, m , r = map(int, input().split())
+# 간선 정보를 담아줄 2차원 배열 생성
 graph = [[] for _ in range(n + 1)]
 visited = [0] * (n + 1)
-cnt = 1
+order = 1
 
 def dfs(v):
-    global cnt
-    visited[v] = cnt
+    global order
+    visited[v] = order
     graph[v].sort(reverse=True)
+    # graph [[], [4, 2], [4, 3, 1], [4, 2], [3, 2, 1], []]
     for i in graph[v]:
         if visited[i] == 0:
-            cnt += 1
+            order += 1
             dfs(i)
 
 for i in range(m):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
+# graph [[], [4, 2], [1, 3, 4], [2, 4], [1, 2, 3], []]
 
 dfs(r)
+# visited [0, 1, 4, 3, 2, 0]
 for i in range(1, n + 1):
     print(visited[i])
