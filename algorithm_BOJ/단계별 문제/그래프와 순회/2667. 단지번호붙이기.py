b6a02adb70787415e118d3e65334e882c1300784
helen121027 <helen121027@gmail.com>
2023-02-12 17:29:29 +0900

    230212 2667 단지번호붙이기

---
+++ algorithm_BOJ/단계별 문제/그래프와 순회/2667. 단지번호붙이기.py
@@ -0,0 +1,34 @@
+n = int(input())
+data = [list(map(int, input())) for i in range(n)]
+visited = [[False] * n for _ in range(n)]
+ans = []
+cnt = 0
+
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+def dfs(x, y):
+    global cnt
+    visited[x][y] = True
+    if data[x][y] == 1:
+        cnt += 1
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < n and 0 <= ny < n:
+            if visited[nx][ny] == False and data[nx][ny] == 1:
+                dfs(nx, ny)
+
+for i in range(n):
+    for j in range(n):
+        if data[i][j] == 1 and visited[i][j] == False:
+            dfs(i, j)
+            ans.append(cnt)
+            cnt = 0
+ans.sort()
+print(len(ans))
+for i in ans:
+    print(i)
+
+
+
