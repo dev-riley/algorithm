5b10eabff56968ea313a56b52f4ba2e8e51ebd8b
helen121027 <helen121027@gmail.com>
2023-03-12 00:13:29 +0900

    230311 1924 2007년

---
+++ algorithm_BOJ/유형별 문제/입출력/1924. 2007년.py
@@ -0,0 +1,11 @@
+x, y = map(int, input().split())
+
+day = 0
+calList = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+weekList = ["SUN", "MON","TUE", "WED", "THU", "FRI", "SAT"]
+
+for i in range(x-1):
+    day = day + calList[i]
+day = (day + y) % 7
+
+print(weekList[day])
