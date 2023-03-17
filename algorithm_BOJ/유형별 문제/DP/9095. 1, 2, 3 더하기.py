20e4efbbbac99de91f87bcfd043d691856bd8c37
helen121027 <helen121027@gmail.com>
2023-03-17 23:29:25 +0900

    230317 9095 1, 2, 3 더하기

---
+++ algorithm_BOJ/유형별 문제/DP/9095. 1, 2, 3 더하기.py
@@ -0,0 +1,14 @@
+T = int(input())
+dp = [0] * 12
+
+for i in range(T):
+    n = int(input())
+
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 4
+
+    for j in range(4, n + 1):
+        dp[j] = dp[j-3] + dp[j-2] + dp[j-1]
+
+    print(dp[n])
